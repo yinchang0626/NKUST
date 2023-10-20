9d6161768b6aaf1d6b71e9e248f2d78c08f5155d
YinChang <yinchang0626@gmail.com>
2023-10-20 12:00:42 +0800

    feat:first console sample

---
+++ Sample/src/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp.Models;
+using ConsoleApp.Services;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ImportXmlService xmlService = new ImportXmlService();
+
+            List<StationExit> datas = xmlService.LoadFormFile(Utils.FilePath.GetFullPath("北捷站點.xml"));
+
+            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", datas.Count));
+
+            datas.ForEach(x =>
+            {
+                Console.WriteLine(string.Format("編號 :{0} 名稱:{1}({2}) 描述:{3}", x.StationID, x.StationName, x.ExitName, x.LocationDescription));
+            });
+
+
+            var jsonService = new ImportJsonService();
+
+
+            var jsonDatas = jsonService.LoadFormFile(Utils.FilePath.GetFullPath("高雄活動.json"));
+
+            Console.WriteLine(string.Format("分析完成，共有{0}筆資料", jsonDatas.Count));
+            jsonDatas.ForEach(x =>
+            {
+                Console.WriteLine(string.Format("編號 :{0} 名稱:{1} 地點:{2}", x.Id, x.DisplayName, x.Kind));
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sample/src/ConsoleApp/Services/ImportXmlService.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp.Services
+{
+    public class ImportXmlService
+    {
+        public ImportXmlService()
+        {
+        }
+
+        public List<StationExit> LoadFormFile(string filePath)
+        {
+            var str = System.IO.File.ReadAllText(filePath);
+
+            XDocument? xDocument = System.Xml.Linq.XDocument.Parse(str);
+
+            if (xDocument == null) return new List<StationExit>();
+            List<XElement> targets = xDocument.Descendants("StationExit").ToList();
+
+            return targets
+                .Select(x =>
+                {
+                    var item = new StationExit();
+                    item.StationID = x.Element("StationID").Value;
+                    item.UpdateTime = x.Element("UpdateTime").Value;
+                    item.LocationDescription = x.Element("LocationDescription").Value;
+                    item.Elevator = bool.Parse(x.Element("Elevator").Value);
+                    var stationNameNode = x.Element("StationName");
+                    var exitNameNode = x.Element("ExitName");
+                    item.StationName = stationNameNode.Element("Zh_tw").Value;
+                    item.ExitName = exitNameNode.Element("Zh_tw").Value;
+                    return item;
+                })
+                .ToList();
+        }
+    }
+}
