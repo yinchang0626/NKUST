e95eb86620838fc4270d1e260bc7515343896a8b
YinChang <yinchang0626@gmail.com>
2024-05-30 12:38:11 +0800

    replace layout with adminlte

---
+++ Sample/src/Sample/Controllers/Api/ActivitiesApiController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Services;
+
+namespace Sample.Controllers
+{
+    [Route("api/activities-api")]
+    [ApiController]
+    public class ActivitiesApiController : ControllerBase
+    {
+        private readonly ActivityService activityService;
+
+        public ActivitiesApiController(
+            ActivityService activityService
+            )
+        {
+            this.activityService = activityService;
+        }
+
+        [HttpPost]
+        [Route("apply/{prgId}")]
+        public IActionResult Apply(long prgId)
+        {
+            this.activityService.Apply(prgId, User?.Identity?.Name);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("cancel-apply/{prgId}")]
+        public IActionResult CancelApply(long prgId)
+        {
+            this.activityService.CancelApply(prgId, User?.Identity?.Name);
+            return Ok();
+        }
+    }
+}
